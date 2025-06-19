1851f7509cae608ef5f2cbaf7f952ee6ae317d47
PeterKimLiz <pkironji@yahoo.com>
2025-06-19 12:33:19 +0300

    added Movie Api and the corresponding dtos

---
+++ app/src/main/java/com/example/movie/movieList/data/remote/MovieApi.kt
@@ -0,0 +1,21 @@
+package com.example.movie.movieList.data.remote
+
+import com.example.movie.movieList.data.dtos.MovieResponseDto
+import com.example.movie.utils.secrets.Secrets.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApi {
+
+  @GET("/movie/{category}")
+   suspend fun getMovies(
+      @Path("category") category:String,
+      @Query("page") page:Int,
+      @Query("api_key") apiKey:String=API_KEY,
+
+      ): MovieResponseDto
+
+
+
+}
---
+++ app/src/main/java/com/example/movie/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.movie.utils
+
+object Constants {
+   const val BASE_URL = "https://api.themoviedb.org/3/"
+   const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+   const val POPULAR="popular"
+   const val UPCOMING="upcoming"
+
+}
